2c450d26f66ef5b4ebc4331a1883e96aae4a485d
lmike <lmike@amazon.com>
2017-11-17 20:56:57 -0800

    add neovim configs

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 echo Install vim-plug
+# for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# for neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
 echo Dotfiles
 ln -sf ${PWD}/vimrc ~/.vimrc
+ln -sf ${PWD}/vimrc ~/.config/nvim/init.vim
 ln -sf ${PWD}/gitconfig ~/.gitconfig
 ln -sf ${PWD}/tmux.conf ~/.tmux.conf
 ln -sf ${PWD}/ctags ~/.ctags
